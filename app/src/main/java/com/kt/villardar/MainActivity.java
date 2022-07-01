1e9e2e04fcb7f1e1b4c189a9aa957521541d5951
kaimatzu <karljohnvillardar@gmail.com>
2022-07-01 18:47:56 +0800

    Milestone 3: Add Event Listener

--- app/src/main/java/com/kt/villardar/MainActivity.java
+++ app/src/main/java/com/kt/villardar/MainActivity.java
@@ -23,6 +23,24 @@ public class MainActivity extends AppCompatActivity {
                 R.array.choice_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+
+        ConstraintLayout constraintLayout = (ConstraintLayout) findViewById(R.id.constLayout);
+        constraintLayout.setBackgroundColor(Color.WHITE);
+        myButtonListenerMethod();
     }
 
+    public void myButtonListenerMethod(){
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                ConstraintLayout constraintLayout = (ConstraintLayout) findViewById(R.id.constLayout);
+                int color = ((ColorDrawable)constraintLayout.getBackground()).getColor();
+                if(color == Color.RED)
+                    constraintLayout.setBackgroundColor(Color.WHITE);
+                else
+                    constraintLayout.setBackgroundColor(Color.RED);
+            }
+        });
+    }
 }
